c0207c05933ab1e351d481b0a6caf2d3e3646c6f
congzw <46074987@qq.com>
2023-03-28 15:37:39 +0800

    Add-Migration EmptyDb

---
+++ WebApp.Migrate/Migrations/20230328073637_EmptyDb.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NbSites.Web.Migrations
+{
+    public partial class EmptyDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
